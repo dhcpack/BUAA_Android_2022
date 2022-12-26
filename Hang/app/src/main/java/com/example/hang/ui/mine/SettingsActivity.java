f2b238875c0feb807daec613398693b5ccff1001
Siazxyyy <zhangxiaoyasia@qq.com>
2022-12-26 23:15:36 +0800

    update

--- Hang/app/src/main/java/com/example/hang/ui/mine/SettingsActivity.java
+++ Hang/app/src/main/java/com/example/hang/ui/mine/SettingsActivity.java
@@ -1,20 +1,56 @@
 package com.example.hang.ui.mine;
 
-import android.os.Bundle;
+import static android.app.UiModeManager.MODE_NIGHT_YES;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.hang.R;
-
-import java.util.ArrayList;
+import com.example.hang.ui.mine.utils.view.SwitchButton;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_report);
-        String username = getIntent().getStringExtra("username");
-        ArrayList<String> params = new ArrayList<>();
-        params.add(username);
+        setContentView(R.layout.activity_settings);
+
+        setTitleBar("设置");
+        SwitchButton btn_turn_on_night_mode = findViewById(R.id.btn_turn_on_night_mode);
+        btn_turn_on_night_mode.setOnCheckedChangeListener((button, checked) -> {
+            if (btn_turn_on_night_mode.isChecked()) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        });
+    }
+
+    public void setTitleBar(String title) {
+        //设置顶部菜单栏
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            actionBar.setCustomView(R.layout.title_layout);//设置标题样式
+            TextView textView = (TextView) actionBar.getCustomView().findViewById(R.id.display_title);//获取标题布局的textview
+            textView.setText(title);//设置标题名称，menuTitle为String字符串
+            actionBar.setHomeButtonEnabled(true);//设置左上角的图标是否可以点击
+            actionBar.setDisplayHomeAsUpEnabled(true);//给左上角图标的左边加上一个返回的图标
+            actionBar.setDisplayShowCustomEnabled(true);//使自定义的普通View能在title栏显示，即actionBar.setCustomView能起作用
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
