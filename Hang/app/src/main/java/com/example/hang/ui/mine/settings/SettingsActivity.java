29f52d05a70a92d80e6a3e7ad49ed32bfd12be0a
Siazxyyy <zhangxiaoyasia@qq.com>
2022-12-28 10:59:14 +0800

    update

--- Hang/app/src/main/java/com/example/hang/ui/mine/settings/SettingsActivity.java
+++ Hang/app/src/main/java/com/example/hang/ui/mine/settings/SettingsActivity.java
@@ -1,11 +1,10 @@
 package com.example.hang.ui.mine.settings;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
-import android.view.View;
+
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
---
+++ Hang/app/src/main/java/com/example/hang/ui/mine/utils/function/GlideEngine.java
@@ -0,0 +1,152 @@
+//package com.example.hang.ui.mine.utils.function;
+//
+//import android.content.Context;
+//import android.graphics.Bitmap;
+//import android.graphics.drawable.Drawable;
+//import android.widget.ImageView;
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import com.bumptech.glide.Glide;
+//import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+//import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+//import com.bumptech.glide.request.target.CustomTarget;
+//import com.bumptech.glide.request.transition.Transition;
+//import com.luck.picture.lib.engine.ImageEngine;
+//import com.luck.picture.lib.interfaces.OnCallbackListener;
+//import com.luck.picture.lib.utils.ActivityCompatHelper;
+//
+///**
+// * Glide图片加载工具类
+// *
+// * 本文作者：谷哥的小弟
+// * 博客地址：http://blog.csdn.net/lfdfhl
+// */
+//public class GlideEngine implements ImageEngine {
+//    private static GlideEngine instance;
+//
+//    private GlideEngine() {}
+//
+//    // 单例
+//    public static GlideEngine createGlideEngine() {
+//        if (null == instance) {
+//            synchronized (GlideEngine.class) {
+//                if (null == instance) {
+//                    instance = new GlideEngine();
+//                }
+//            }
+//        }
+//        return instance;
+//    }
+//
+//    /**
+//     * 加载图片
+//     *
+//     * @param context   上下文
+//     * @param url       资源url
+//     * @param imageView 图片承载控件
+//     */
+//    @Override
+//    public void loadImage(@NonNull Context context, @NonNull String url, @NonNull ImageView imageView) {
+//        if (!ActivityCompatHelper.assertValidRequest(context)) {
+//            return;
+//        }
+//        Glide.with(context)
+//                .load(url)
+//                .into(imageView);
+//    }
+//
+//    /**
+//     * 加载指定url并返回bitmap
+//     *
+//     * @param context   上下文
+//     * @param url       资源url
+//     * @param maxWidth  资源最大加载尺寸
+//     * @param maxHeight 资源最大加载尺寸
+//     * @param call      回调接口
+//     */
+//    @Override
+//    public void loadImageBitmap(@NonNull Context context, @NonNull String url, int maxWidth, int maxHeight, OnCallbackListener<Bitmap> call) {
+//        if (!ActivityCompatHelper.assertValidRequest(context)) {
+//            return;
+//        }
+//        Glide.with(context)
+//                .asBitmap()
+//                .override(maxWidth, maxHeight)
+//                .load(url)
+//                .into(new CustomTarget<Bitmap>() {
+//                    @Override
+//                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+//                        if (call != null) {
+//                            call.onCall(resource);
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onLoadFailed(@Nullable Drawable errorDrawable) {
+//                        if (call != null) {
+//                            call.onCall(null);
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onLoadCleared(@Nullable Drawable placeholder) {
+//
+//                    }
+//
+//                });
+//    }
+//
+//    /**
+//     * 加载相册目录封面
+//     *
+//     * @param context   上下文
+//     * @param url       图片路径
+//     * @param imageView 承载图片ImageView
+//     */
+//    @Override
+//    public void loadAlbumCover(@NonNull Context context, @NonNull String url, @NonNull ImageView imageView) {
+//        if (!ActivityCompatHelper.assertValidRequest(context)) {
+//            return;
+//        }
+//        Glide.with(context)
+//                .asBitmap()
+//                .load(url)
+//                .override(180, 180)
+//                .sizeMultiplier(0.5f)
+//                .transform(new CenterCrop(), new RoundedCorners(8))
+//                .into(imageView);
+//    }
+//
+//
+//    /**
+//     * 加载图片列表图片
+//     *
+//     * @param context   上下文
+//     * @param url       图片路径
+//     * @param imageView 承载图片ImageView
+//     */
+//    @Override
+//    public void loadGridImage(@NonNull Context context, @NonNull String url, @NonNull ImageView imageView) {
+//        if (!ActivityCompatHelper.assertValidRequest(context)) {
+//            return;
+//        }
+//        Glide.with(context)
+//                .load(url)
+//                .override(200, 200)
+//                .centerCrop()
+//                .into(imageView);
+//    }
+//
+//    @Override
+//    public void pauseRequests(Context context) {
+//        Glide.with(context).pauseRequests();
+//    }
+//
+//    @Override
+//    public void resumeRequests(Context context) {
+//        Glide.with(context).resumeRequests();
+//    }
+//
+//
+//}
+//
